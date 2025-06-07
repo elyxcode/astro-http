26e2d01948bc4f360d4a0aec373175bbcb69a7ea
Douglas Juárez <dougjuarez21.dev@gmail.com>
2025-06-07 17:48:29 -0600

    remove node integration and add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
+
+// import node from "@astro js/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
   integrations: [mdx(), sitemap()],
-  output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
+
+  adapter: cloudflare(),
 });
