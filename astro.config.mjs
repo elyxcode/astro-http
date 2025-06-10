9cb8a40e4e2810d1fe57051eebf04186ca55fae9
Douglas Juárez <dougjuarez21.dev@gmail.com>
2025-06-10 16:46:24 -0600

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 import db from "@astrojs/db";
 
-// import node from "@astro js/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
   integrations: [mdx(), sitemap(), db()],
-
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
